84d0951e3d6556a8f7d584f261b6691e46a747fc
amits-space <arajhzb567@gmail.com>
2026-05-28 20:40:28 +0530

    Modified the bubble sort code by putting the sorting algorithm in a separate function than main

--- Arrays/bubble_sort/bubble.cpp
+++ Arrays/bubble_sort/bubble.cpp
@@ -1,22 +1,12 @@
+//bubble sort algorithm
+
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
-    cout<<"Enter the size of the array: ";
-    cin>>n;
-    int A[n];
-    cout<<"Enter the elements of the array:\n";
-
-    for(int i=0; i<n; i++){
-        cout<<"A["<<i<<"]: ";
-        cin>>A[i];
-    }
-
-    int temp;
-
-    for (int i=0; i<n-1; i++){
-        for(int j=0; j<n-1-i; j++){
+void sort(int A[], int n){
+    int i, j, temp;
+    for(i=0; i<n; i++){
+        for(j=0; j<n-i-1; j++){
             if(A[j]>A[j+1]){
                 temp=A[j];
                 A[j]=A[j+1];
@@ -24,11 +14,25 @@ int main(){
             }
         }
     }
+}
 
-
+int main(){
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    
+    int A[n];
+    cout<<"Enter the elements of the array\n";
+    for(int i=0; i<n; i++){
+        cin>>A[i];
+    }
+    
+    sort(A, n);
+    
     cout<<"The sorted array is:\n";
-    for(int x: A)
-        cout<<x<<" ";
+    
+    for(int x:A)
+        cout<<x<<endl;
     
     return 0;
 }
